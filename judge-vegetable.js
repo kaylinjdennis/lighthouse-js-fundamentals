b13c6edf6fd7014fe4dba074ab67704ee76fe216
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-01-11 17:27:16 -0700

    Added another practice file

---
+++ judge-vegetable.js
@@ -0,0 +1,35 @@
+const judgeVegetable = function(vegetables, metric) {
+    let winningVal = (vegetables[0])[metric];
+    console.log(winningVal);
+    let winner = (vegetables[0]).submitter;
+    for (let i = 1; i < vegetables.length; i++) {
+        console.log('enter loop');
+        if ((vegetables[i])[metric] > winningVal) {
+            winningVal = (vegetables[i])[metric];
+            winner = (vegetables[i]).submitter;
+        }
+    }
+    return winner;
+}
+
+
+const vegetables = [{
+        submitter: 'Sally Tomato-Grower',
+        redness: 2,
+        plumpness: 8
+    },
+    {
+        submitter: 'Old Man Franklin',
+        redness: 10,
+        plumpness: 5
+    },
+    {
+        submitter: 'Hamid Hamidson',
+        redness: 4,
+        plumpness: 3
+    }
+]
+
+const metric = 'redness'
+
+console.log(judgeVegetable(vegetables, metric));
